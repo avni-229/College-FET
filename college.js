8c9bdff908d586598163298c7f276901bb5b9ca1
dhanyaa <148534876+avni-229@users.noreply.github.com>
2024-04-29 18:10:40 +0530

    Add files via upload

---
+++ college.js
@@ -0,0 +1,22 @@
+const college = {
+    "am-ET": "Artificial Intelligent",
+    "ar-SA": "Mechatronics",
+    "be-BY": "Mechanical Engineering",
+    "bem-ZM": "Civil",
+    "bi-VU": "Naturopathy",
+    "bjs-BB": "Automobile",
+    "bn-IN": "CSE",
+    "bo-CN": "IT",
+    "br-FR": "BSC-CSD",
+    "bs-BA": "BSC-IT",
+    "ca-ES": "BSC-SS",
+    "cop-EG": "ECE",
+    "cs-CZ": "EEE",
+    "cy-GB": "EIE",
+    "da-DK": "FOOD-TECH",
+    "dz-BT": "CHEMICAL",
+    "de-DE": "MSC",
+    "dv-MV": "MBA",
+    "el-GR": "MCA",
+    
+}
